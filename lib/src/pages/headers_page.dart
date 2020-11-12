45188e8cb27b9e7baaeda4234d11923fac5f2125
CristianX <thecristianx@hotmail.com>
2020-11-11 19:11:23 -0500

    Implementando header curvo con custom painter

--- lib/src/pages/headers_page.dart
+++ lib/src/pages/headers_page.dart
@@ -7,7 +7,7 @@ class HeadersPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: HeaderPico(),
+      body: HeaderCurvo(),
       
     );
   }
