30a22637940323127c099f71bf5a41b55753f7fe
CristianX <thecristianx@hotmail.com>
2020-11-11 19:49:17 -0500

    Implementando Header Waves con gradiente utilizando custom painter y gradiente

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -290,3 +290,78 @@ class _HeaderWavesPainter extends CustomPainter {
 
 }
 
+class HeaderWavesGradiente extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      // 0xff para especificar un color hexadecimal
+      // color: Color(0xff615AAB),
+      child: CustomPaint(
+        painter: _HeaderWavesPainterGradiente(),
+      ),
+    );
+  }
+}
+
+class _HeaderWavesPainterGradiente extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final Rect rect = new Rect.fromCircle(
+      center: Offset(0, 55),
+      radius: 180
+    );
+
+    final Gradient gradiente = new LinearGradient(
+      // Definición vertical del gradiente
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      colors: <Color>[
+        Color(0xff6D05E8),
+        Color(0xffC012FF),
+        Color(0xff6D05FA),
+      ],
+      // Puntos donde trabajara el gradiente
+      stops: [
+        0.2,
+        0.5,
+        1.0,
+      ]
+    );
+
+    // lapiz que se utilizará para pintar (..shader es para la definición del gradiente)
+    final lapiz = Paint()..shader = gradiente.createShader(rect);
+
+    // Propiedades
+    // lapiz.color = Color(0xff615AAB);
+    lapiz.strokeWidth = 20;
+    lapiz.style = PaintingStyle.fill; // .fill rellenar  .stroke solo bordes
+    // Forma en la qu va a dibujar
+    final path = new Path();
+
+    // Dibujar con el path y el lapiz
+    path.lineTo(0, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.30, size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.20, size.width, size.height * 0.25);
+    path.lineTo(size.width, 0);
+    // path.lineTo(size.width, size.height * 0.25);
+
+    
+
+
+
+    // Diciendole a canvas que dibuje
+    canvas.drawPath(path, lapiz);
+
+
+  }
+  
+    @override
+    bool shouldRepaint(covariant CustomPainter oldDelegate) {
+      return true;
+  }
+
+}
+
