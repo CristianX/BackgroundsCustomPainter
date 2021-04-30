bf4d656c6aa147e500d1517ae50f628763af5d8c
CristianX <thecristianx@hotmail.com>
2021-04-29 22:03:30 -0500

    Implementación de animaciones (Animation y AnimationController) en la vista animaciones_page

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -16,7 +16,38 @@ class CuadradoAnimado extends StatefulWidget {
   _CuadradoAnimadoState createState() => _CuadradoAnimadoState();
 }
 
-class _CuadradoAnimadoState extends State<CuadradoAnimado> {
+class _CuadradoAnimadoState extends State<CuadradoAnimado> with SingleTickerProviderStateMixin {
+
+  // Para animar cualquier cosa
+
+  //   AnimationController Sirve para manejar la animación
+  AnimationController controller ; //Barra que muestra la animación de avance como videos de youtube
+
+  // Animación (Que tipo de cosa yo quiero animar?)
+  Animation<double> rotacion;
+
+  // Inicializar los dos objetos de Animation
+  @override
+  void initState() {
+
+    // El state debe tener SingleTickerProviderStateMixin para controlar el vsync
+    controller = new AnimationController(vsync: this, duration: Duration(milliseconds: 4000));
+
+    // Configurando rotación
+    rotacion = Tween( begin: 0.0, end: 2.0 ).animate( controller );
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Siempre se debe eliminar el controller en el dispose ya que es como un observable (siempre escuchará los cambios)
+    // Se lo detiene para que no exista una fuga de memoria
+    controller.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return _Rectangulo();
