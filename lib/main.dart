1a11452ad630123df0f38e5b88d1b557778cfa89
CristianX <thecristianx@hotmail.com>
2021-05-01 23:36:24 -0500

    Creación de una nueva vista (circular_progress_page) y un container

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:disenios_app/src/pages/circular_progress_page.dart';
 import 'package:flutter/material.dart';
 
-import 'package:disenios_app/src/retos/cuadrado_animado_page.dart';
+// import 'package:disenios_app/src/retos/cuadrado_animado_page.dart';
 
 // import 'package:disenios_app/src/pages/animaciones_page.dart';
 // import 'package:disenios_app/src/pages/headers_page.dart';
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños App',
-      home: CuadradoAnimadoPage()
+      home: CircularProgressPage()
     );
   }
 }
