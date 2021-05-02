72de8ad022f408b6270ae5a03b374e250d53cb1b
CristianX <thecristianx@hotmail.com>
2021-05-02 18:41:15 -0500

    Mostrando circulo y arco para el circular progress junto con su operación

--- lib/src/widgets/radial_progress.dart
+++ lib/src/widgets/radial_progress.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class RadialProgress extends StatefulWidget {
@@ -16,8 +18,59 @@ class _RadialProgressState extends State<RadialProgress> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text( '${widget.porcentaje}' ),
-      
+      padding: EdgeInsets.all(10),
+      width: double.infinity,
+      height: double.infinity,
+      child: CustomPaint(
+        painter: _MiRadialProgress( widget.porcentaje ),
+      ),
     );
   }
+}
+
+class _MiRadialProgress extends CustomPainter {
+
+  final porcentaje;
+
+  _MiRadialProgress( this.porcentaje );
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+
+    // Circulo completado
+    // Definiendo lapiz
+    final paint = new Paint()
+      ..strokeWidth = 4
+      .. color = Colors.grey
+      .. style = PaintingStyle.stroke;
+
+    final center = new Offset( size.width * 0.5 , size.height * 0.5);
+    final radio = min(size.width  * 0.5, size.height * 0.5);
+    
+    canvas.drawCircle(center, radio, paint);
+
+    // Arco
+    final paintArco = new Paint()
+      ..strokeWidth = 10
+      .. color = Colors.pink
+      .. style = PaintingStyle.stroke;
+
+      // Parte que se deberá ir llenando
+      // 2 * pi es una vuelta completa
+      double arcAngle = 2 * pi * ( porcentaje / 100 );
+      canvas.drawArc(
+        Rect.fromCircle( center: center, radius: radio ), 
+        -pi /2, 
+        arcAngle, 
+        false, 
+        paintArco
+      );
+
+
+  }
+  
+    @override
+    bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+
 }
