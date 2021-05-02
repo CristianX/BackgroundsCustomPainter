3881071bb775373e9cd33e98b07e25dca9200c25
CristianX <thecristianx@hotmail.com>
2021-05-02 18:35:31 -0500

    Implementando widget personalizado radial_progress

--- lib/src/pages/graficas_circulares_page.dart
+++ lib/src/pages/graficas_circulares_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:disenios_app/src/widgets/radial_progress.dart';
 
 
 class GraficasCircularesPage extends StatefulWidget {
@@ -26,7 +27,13 @@ class _GraficasCircularesPageState extends State<GraficasCircularesPage> {
         },
       ),
       body: Center(
-        child: Text('$porcentaje %', style: TextStyle( fontSize: 50.0 )),
+        child: Container(
+          width: 300,
+          height: 300,
+          color: Colors.red,
+          child: RadialProgress( porcentaje: 40.0 ),
+          // child: Text('$porcentaje %', style: TextStyle( fontSize: 50.0 )),
+        ),
       ),      
     );
   }
