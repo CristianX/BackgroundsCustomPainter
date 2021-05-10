fe16b813c2cc7b1a224cd49fc6f823816f63b7b4
CristianX <thecristianx@hotmail.com>
2021-05-09 22:32:49 -0500

    Mostrando íconos en el widget de pinteres_menu

--- lib/src/widgets/pinterest_menu.dart
+++ lib/src/widgets/pinterest_menu.dart
@@ -24,8 +24,54 @@ class PinterestMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        child: Text( 'Hola mundo desde el menu' ),
+
+        child: _MenuItems( items ),
+
+        width: 250,
+        height: 60,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all( Radius.circular(100) ),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.black38,
+              // offset: Offset(10, 10),
+              blurRadius: 10,
+              spreadRadius: -5
+            )
+          ]
+        ),
       ),
+    );
+  }
+}
+
+class _MenuItems extends StatelessWidget {
+
+  final List<PinterestButton> menuItems;
+
+  _MenuItems( this.menuItems );
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(menuItems.length, (i) => _PinterestMenuButton( i, menuItems[i] ))
+    );
+  }
+}
+
+class _PinterestMenuButton extends StatelessWidget {
+
+  final int index;
+  final PinterestButton item;
+
+  _PinterestMenuButton( this.index, this.item );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon( item.icon ),
     );
   }
 }
