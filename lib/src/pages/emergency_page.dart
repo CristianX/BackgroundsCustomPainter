e10a05db29451032116c68b780259b285ddb3adf
CristianX <thecristianx@hotmail.com>
2021-07-27 15:00:06 -0500

    Añadiendo información dentro del boton

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -10,6 +10,7 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.red,
       body: Center(
         child: BotonGordo(),
       )
--- lib/src/widgets/boton_gordo.dart
+++ lib/src/widgets/boton_gordo.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BotonGordo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _BotonGordoBackground();
+    return Stack(
+      children: <Widget>[
+        _BotonGordoBackground(),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox( height: 140, width: 40.0 ),
+            FaIcon( FontAwesomeIcons.carCrash, color: Colors.white, size: 40.0 ),
+            SizedBox( width: 20.0 ),
+            Expanded(child: Text('Motor Accident', style: TextStyle( color: Colors.white, fontSize: 18.0 ))),
+            FaIcon( FontAwesomeIcons.chevronRight, color: Colors.white ),
+            SizedBox( width: 40 )
+          ],
+        )
+      ],
+    );
   }
 }
 
@@ -13,6 +30,21 @@ class _BotonGordoBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+
+      // ClipRRect sirve para cortar los bordes de lo que está contenido
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              right: -20.0,
+              top: -20.0,
+              child: FaIcon( FontAwesomeIcons.carCrash, size: 150.0, color: Colors.white.withOpacity(0.2)),
+            )
+          ],
+        ),
+      ),
+
       width: double.infinity,
       height: 100,
       margin: EdgeInsets.all( 20.0 ),
