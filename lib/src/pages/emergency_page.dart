97ab83d2d230b5b54048cdad2f011ed7e5dd5d72
CristianX <thecristianx@hotmail.com>
2021-07-27 14:44:22 -0500

    Creando contenedor del boton

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,19 +1,35 @@
-import 'package:disenios_app/src/widgets/headers.dart';
 import 'package:flutter/material.dart';
+
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:disenios_app/src/widgets/headers.dart';
+import 'package:disenios_app/src/widgets/boton_gordo.dart';
 
 class EmergencyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Iconheader(
-        icon: FontAwesomeIcons.plus,
-        subtitulo: 'Asistencia Médica',
-        titulo: 'Haz Solicitado',
-        color1: Color(0xff526bf6),
-        color2: Color(0xff67acf2),
-      ),
+      body: Center(
+        child: BotonGordo(),
+      )
+    );
+  }
+}
+
+class PageHeader extends StatelessWidget {
+  const PageHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Iconheader(
+      icon: FontAwesomeIcons.plus,
+      subtitulo: 'Asistencia Médica',
+      titulo: 'Haz Solicitado',
+      color1: Color(0xff526bf6),
+      color2: Color(0xff67acf2),
     );
   }
 }
